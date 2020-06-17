3bb6cbf4fb160ec3bd125e4e0ba65e7c7ac99993
José Carlos <jcolscript@gmail.com>
2020-06-17 00:30:24 -0300

    adicionando itens ao carrinho

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
+import { connect } from 'react-redux';
 
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-export default function index() {
+function Header({ cart }) {
   return (
     <Container>
       <Link to="/">
@@ -16,10 +17,14 @@ export default function index() {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span>{cart.length} itens</span>
         </div>
         <MdShoppingBasket size={36} color="#fff" />
       </Cart>
     </Container>
   );
 }
+
+export default connect((state) => ({
+  cart: state.cart,
+}))(Header);
